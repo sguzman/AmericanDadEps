f17844b9e561de574d72499e34ac0bca48943e48
Salvador Guzman <guzmansalv@gmail.com>
2018-02-13 19:25:46 -0800

    use single video layout instead

--- src/main/scala/com/github/sguzman/scala/js/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/Main.scala
@@ -1,8 +1,8 @@
 package com.github.sguzman.scala.js
 
-import com.thoughtworks.binding.Binding.Var
+import com.thoughtworks.binding.Binding.{Var, Vars}
 import com.thoughtworks.binding.{Binding, dom}
-import org.scalajs.dom.html.{Div, Input}
+import org.scalajs.dom.html.Div
 import org.scalajs.dom.{Event, document}
 
 object Main {
@@ -10,24 +10,20 @@ object Main {
     def id[A] = document.getElementById(str).asInstanceOf[A]
   }
 
-  @dom def _render(s: Var[String]): Binding[Div] = {
+  implicit final class IntWrap(x: Int) {
+    def isZero = x == 0
+    def isNonZero = x != 0
+  }
+
+  @dom def _render(s: Vars[String], x: Var[Int]): Binding[Div] = {
     <div>
-      <button disabled={s.bind.isEmpty} ></button>
-      <input id="input" oninput={_: Event => s.value_=("input".id[Input].value) } />
-      <p>{s.bind}</p>
-      <table>
-        <tbody>
-          {
-            for (vid <- RawFiles.list) yield {
-              <video src={vid} ></video>
-            }
-          }
-        </tbody>
-      </table>
+      <button disabled={x.bind.isZero} onclick={_: Event => x.value -= 1} >Previous</button>
+      <video src={s.value(x.bind)}></video>
+      <button disabled={x.bind >= s.value.length} onclick={_: Event => x.value += 1} >Next</button>
     </div>
   }
 
-  def render = _render(Var(""))
+  def render = _render(RawFiles.list, Var(0))
 
   def main(args: Array[String]): Unit = dom.render(document.body, render)
 }
